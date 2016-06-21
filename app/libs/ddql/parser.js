36966d620449d47e7fe28a9eb5b23ce6ed337511
Cheung Chifung <chifung.cheung@geniee.co.jp>
2016-06-22 00:17:20 +0900

    backup

--- app/libs/ddql/parser.js
+++ app/libs/ddql/parser.js
@@ -16,13 +16,24 @@ const selectAction = lexeme(P.regex(/SELECT/i))
 
 const fromClause = lexeme(P.regex(/FROM/i))
 const whereClause = lexeme(P.regex(/WHERE/i))
+const andClause = lexeme(P.regex(/AND/i))
+const orClause = lexeme(P.regex(/OR/i))
+const notClause = lexeme(P.regex(/NOT/i))
+const inClause = lexeme(P.regex(/IN/i))
+const betweenClause = lexeme(P.regex(/BETWEEN/i))
 const semicolonClause = lexeme(P.string(';'))
+const commaClause = lexeme(P.string(','))
 
 // const actionName = lexeme(P.regex(/SELECT|INSERT|UPDATE|DELETE/i)).map((n) => n)
 
 const number = lexeme(P.regex(/[0-9]+/).map(parseInt))
+const string = lexeme(P.regex(/`[^`\\]*(?:\\.[^`\\]*)*`/))
 
 const tableName = lexeme(P.regex(/[a-zA-Z0-9_\-\.]{3,255}/))
+
+// const functionExpression = lexeme(
+//   P.regex(/attribute_exists|attribute_not_exists|begins_with|contains/i)
+// )
 
 const reservedWordAttributeName = lexeme(
   P.regex(/ABORT|ABSOLUTE|ACTION|ADD|AFTER|AGENT|AGGREGATE|ALL|ALLOCATE|ALTER|ANALYZE|AND|ANY|ARCHIVE|ARE|ARRAY|AS|ASC|ASCII|ASENSITIVE|ASSERTION|ASYMMETRIC|AT|ATOMIC|ATTACH|ATTRIBUTE|AUTH|AUTHORIZATION|AUTHORIZE|AUTO|AVG|BACK|BACKUP|BASE|BATCH|BEFORE|BEGIN|BETWEEN|BIGINT|BINARY|BIT|BLOB|BLOCK|BOOLEAN|BOTH|BREADTH|BUCKET|BULK|BY|BYTE|CALL|CALLED|CALLING|CAPACITY|CASCADE|CASCADED|CASE|CAST|CATALOG|CHAR|CHARACTER|CHECK|CLASS|CLOB|CLOSE|CLUSTER|CLUSTERED|CLUSTERING|CLUSTERS|COALESCE|COLLATE|COLLATION|COLLECTION|COLUMN|COLUMNS|COMBINE|COMMENT|COMMIT|COMPACT|COMPILE|COMPRESS|CONDITION|CONFLICT|CONNECT|CONNECTION|CONSISTENCY|CONSISTENT|CONSTRAINT|CONSTRAINTS|CONSTRUCTOR|CONSUMED|CONTINUE|CONVERT|COPY|CORRESPONDING|COUNT|COUNTER|CREATE|CROSS|CUBE|CURRENT|CURSOR|CYCLE|DATA|DATABASE|DATE|DATETIME|DAY|DEALLOCATE|DEC|DECIMAL|DECLARE|DEFAULT|DEFERRABLE|DEFERRED|DEFINE|DEFINED|DEFINITION|DELETE|DELIMITED|DEPTH|DEREF|DESC|DESCRIBE|DESCRIPTOR|DETACH|DETERMINISTIC|DIAGNOSTICS|DIRECTORIES|DISABLE|DISCONNECT|DISTINCT|DISTRIBUTE|DO|DOMAIN|DOUBLE|DROP|DUMP|DURATION|DYNAMIC|EACH|ELEMENT|ELSE|ELSEIF|EMPTY|ENABLE|END|EQUAL|EQUALS|ERROR|ESCAPE|ESCAPED|EVAL|EVALUATE|EXCEEDED|EXCEPT|EXCEPTION|EXCEPTIONS|EXCLUSIVE|EXEC|EXECUTE|EXISTS|EXIT|EXPLAIN|EXPLODE|EXPORT|EXPRESSION|EXTENDED|EXTERNAL|EXTRACT|FAIL|FALSE|FAMILY|FETCH|FIELDS|FILE|FILTER|FILTERING|FINAL|FINISH|FIRST|FIXED|FLATTERN|FLOAT|FOR|FORCE|FOREIGN|FORMAT|FORWARD|FOUND|FREE|FROM|FULL|FUNCTION|FUNCTIONS|GENERAL|GENERATE|GET|GLOB|GLOBAL|GO|GOTO|GRANT|GREATER|GROUP|GROUPING|HANDLER|HASH|HAVE|HAVING|HEAP|HIDDEN|HOLD|HOUR|IDENTIFIED|IDENTITY|IF|IGNORE|IMMEDIATE|IMPORT|IN|INCLUDING|INCLUSIVE|INCREMENT|INCREMENTAL|INDEX|INDEXED|INDEXES|INDICATOR|INFINITE|INITIALLY|INLINE|INNER|INNTER|INOUT|INPUT|INSENSITIVE|INSERT|INSTEAD|INT|INTEGER|INTERSECT|INTERVAL|INTO|INVALIDATE|IS|ISOLATION|ITEM|ITEMS|ITERATE|JOIN|KEY|KEYS|LAG|LANGUAGE|LARGE|LAST|LATERAL|LEAD|LEADING|LEAVE|LEFT|LENGTH|LESS|LEVEL|LIKE|LIMIT|LIMITED|LINES|LIST|LOAD|LOCAL|LOCALTIME|LOCALTIMESTAMP|LOCATION|LOCATOR|LOCK|LOCKS|LOG|LOGED|LONG|LOOP|LOWER|MAP|MATCH|MATERIALIZED|MAX|MAXLEN|MEMBER|MERGE|METHOD|METRICS|MIN|MINUS|MINUTE|MISSING|MOD|MODE|MODIFIES|MODIFY|MODULE|MONTH|MULTI|MULTISET|NAME|NAMES|NATIONAL|NATURAL|NCHAR|NCLOB|NEW|NEXT|NO|NONE|NOT|NULL|NULLIF|NUMBER|NUMERIC|OBJECT|OF|OFFLINE|OFFSET|OLD|ON|ONLINE|ONLY|OPAQUE|OPEN|OPERATOR|OPTION|OR|ORDER|ORDINALITY|OTHER|OTHERS|OUT|OUTER|OUTPUT|OVER|OVERLAPS|OVERRIDE|OWNER|PAD|PARALLEL|PARAMETER|PARAMETERS|PARTIAL|PARTITION|PARTITIONED|PARTITIONS|PATH|PERCENT|PERCENTILE|PERMISSION|PERMISSIONS|PIPE|PIPELINED|PLAN|POOL|POSITION|PRECISION|PREPARE|PRESERVE|PRIMARY|PRIOR|PRIVATE|PRIVILEGES|PROCEDURE|PROCESSED|PROJECT|PROJECTION|PROPERTY|PROVISIONING|PUBLIC|PUT|QUERY|QUIT|QUORUM|RAISE|RANDOM|RANGE|RANK|RAW|READ|READS|REAL|REBUILD|RECORD|RECURSIVE|REDUCE|REF|REFERENCE|REFERENCES|REFERENCING|REGEXP|REGION|REINDEX|RELATIVE|RELEASE|REMAINDER|RENAME|REPEAT|REPLACE|REQUEST|RESET|RESIGNAL|RESOURCE|RESPONSE|RESTORE|RESTRICT|RESULT|RETURN|RETURNING|RETURNS|REVERSE|REVOKE|RIGHT|ROLE|ROLES|ROLLBACK|ROLLUP|ROUTINE|ROW|ROWS|RULE|RULES|SAMPLE|SATISFIES|SAVE|SAVEPOINT|SCAN|SCHEMA|SCOPE|SCROLL|SEARCH|SECOND|SECTION|SEGMENT|SEGMENTS|SELECT|SELF|SEMI|SENSITIVE|SEPARATE|SEQUENCE|SERIALIZABLE|SESSION|SET|SETS|SHARD|SHARE|SHARED|SHORT|SHOW|SIGNAL|SIMILAR|SIZE|SKEWED|SMALLINT|SNAPSHOT|SOME|SOURCE|SPACE|SPACES|SPARSE|SPECIFIC|SPECIFICTYPE|SPLIT|SQL|SQLCODE|SQLERROR|SQLEXCEPTION|SQLSTATE|SQLWARNING|START|STATE|STATIC|STATUS|STORAGE|STORE|STORED|STREAM|STRING|STRUCT|STYLE|SUB|SUBMULTISET|SUBPARTITION|SUBSTRING|SUBTYPE|SUM|SUPER|SYMMETRIC|SYNONYM|SYSTEM|TABLE|TABLESAMPLE|TEMP|TEMPORARY|TERMINATED|TEXT|THAN|THEN|THROUGHPUT|TIME|TIMESTAMP|TIMEZONE|TINYINT|TO|TOKEN|TOTAL|TOUCH|TRAILING|TRANSACTION|TRANSFORM|TRANSLATE|TRANSLATION|TREAT|TRIGGER|TRIM|TRUE|TRUNCATE|TTL|TUPLE|TYPE|UNDER|UNDO|UNION|UNIQUE|UNIT|UNKNOWN|UNLOGGED|UNNEST|UNPROCESSED|UNSIGNED|UNTIL|UPDATE|UPPER|URL|USAGE|USE|USER|USERS|USING|UUID|VACUUM|VALUE|VALUED|VALUES|VARCHAR|VARIABLE|VARIANCE|VARINT|VARYING|VIEW|VIEWS|VIRTUAL|VOID|WAIT|WHEN|WHENEVER|WHERE|WHILE|WINDOW|WITH|WITHIN|WITHOUT|WORK|WRAPPED|WRITE|YEAR|ZONE/i)
@@ -74,13 +85,13 @@ const projection = P.alt(
 )
 
 const path = P.seq(
-  P.sepBy(
+  P.sepBy1(
     P.alt(
       attributePath,
       hashKeyPath,
       rangeKeyPath
     ),
-    P.string('.')
+    commaClause
   ),
   pathListIndex.many()
 ).map(node => ({
@@ -88,13 +99,8 @@ const path = P.seq(
   'index': node[1]
 }))
 
-// const lparen = lexeme(P.string('('));
-// const rparen = lexeme(P.string(')'));
-
-// const condExpr = P.lazy(
-//   'a condition expression',
-//   () => condForm.or(condOperand)
-// )
+const lparen = lexeme(P.string('('))
+const rparen = lexeme(P.string(')'))
 
 // const operand = P.alt(
 // )
@@ -128,18 +134,129 @@ const conditionComparator = lexeme(P.alt(
   P.string('>=')
 ))
 
-const condition = P.seq(
+const dataType = lexeme(P.alt(
+  P.regex(/SS|stringset/i).map(() => 'StringSet'),
+  P.regex(/S|string/i).map(() => 'String'),
+  P.regex(/NS|numberset/i).map(() => 'NumberSet'),
+  P.regex(/NULL/i).map(() => 'Null'),
+  P.regex(/N|number/i).map(() => 'Number'),
+  P.regex(/BS|binaryset/i).map(() => 'BinarySet'),
+  P.regex(/BOOL|boolean/i).map(() => 'Bool'),
+  P.regex(/B|binary/i).map(() => 'Binary'),
+  P.regex(/L|list/i).map(() => 'List'),
+  P.regex(/M|map/i).map(() => 'Map')
+))
+
+const conditionFunctionAttributeExists = P.seq(
+  lexeme(P.regex(/attribute_exists|exists/i)),
+  lparen,
   path,
-  conditionComparator,
-  number
+  rparen
 )
+
+const conditionFunctionAttributeNotExists = P.seq(
+  lexeme(P.regex(/attribute_not_exists|notexists/i)),
+  lparen,
+  path,
+  rparen
+)
+
+const conditionFunctionAttributeType = P.seq(
+  lexeme(P.regex(/attribute_type|type/i)),
+  lparen,
+  path,
+  commaClause,
+  dataType,
+  rparen
+)
+
+const conditionFunctionBeginsWith = P.seq(
+  lexeme(P.regex(/begins_with|beginswith/)),
+  lparen,
+  path,
+  commaClause,
+  string,
+  rparen
+)
+
+// (a and b) and c
+// a and (b and c)
+// a in [c,d,e] and g > a and exists(k)
+
+const condition = P.lazy(
+  'a condition expression',
+  () => P.sepBy1(
+    P.alt(
+      conditionComparisonExpression,
+      conditionInExpression,
+      conditionBetweenExpression,
+      conditionFunctionExpression,
+      notClause.then(condition),
+      lparen.then(condition.many()).skip(rparen)
+    ),
+    P.alt(
+      andClause,
+      orClause
+    )
+  )
+)
+
+const conditionOperand = P.alt(
+  path
+)
+
+const conditionFunctionExpression = P.alt(
+  conditionFunctionAttributeExists,
+  conditionFunctionAttributeNotExists,
+  conditionFunctionAttributeType,
+  conditionFunctionBeginsWith
+)
+
+const conditionInExpression = P.seq(
+  conditionOperand,
+  inClause,
+  lparen,
+  P.sepBy1(
+    conditionOperand,
+    commaClause
+  ),
+  rparen
+).map(node => ({
+  'clause': 'IN',
+  'operand': node[0],
+  'list': node[3]
+}))
+
+const conditionBetweenExpression = P.seq(
+  conditionOperand,
+  betweenClause,
+  conditionOperand,
+  andClause,
+  conditionOperand
+).map(node => ({
+  'clause': 'BETWEEN',
+  'operand': node[0],
+  'min': node[2],
+  'max': node[4]
+}))
+
+const conditionComparisonExpression = P.seq(
+  conditionOperand,
+  conditionComparator,
+  conditionOperand
+).map(node => ({
+  'clause': 'COMPARE',
+  'leftOperand': node[0],
+  'comparator': node[1],
+  'rightOperand': node[2]
+}))
 
 // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.AccessingItemAttributes.html#DocumentPaths
 
 const selectParser = P.seq(
   selectAction.then(projection),
   fromClause.then(tableName),
-  opt(whereClause.then(path)),
+  opt(whereClause.then(condition)),
   semicolonClause
 ).map(node => ({
   'action': 'select',
@@ -148,12 +265,18 @@ const selectParser = P.seq(
   'where': node[2]
 }))
 
+console.log(conditionFunctionExpression)
+console.log(conditionComparisonExpression)
+console.log(conditionBetweenExpression)
+console.log(conditionInExpression)
 console.log(tableName)
 console.log(path)
+console.log(notClause)
 console.log(whereClause)
 console.log(semicolonClause)
 console.log(pathListIndex)
 console.log(condition)
+console.log(number)
 
 const queryParser = P.alt(
   selectParser
